632eaa89e2b8d303dc9747c46de2fb5dc0242aab
Flavio Reber <reber.flavio@gmail.com>
2024-04-03 20:54:44 +0200

    added avatar + popup

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, type PopupSettings } from '@skeletonlabs/skeleton';
 
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import { storeHighlightJs, Avatar } from '@skeletonlabs/skeleton';
 	import xml from 'highlight.js/lib/languages/xml'; // for HTML
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
@@ -19,15 +19,22 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { storePopup, popup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+
+    const popupCLick: PopupSettings = {
+        event: 'click',
+        target: 'popupClick',
+        placement: 'bottom'
+    };
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar padding="p-1">
 			<svelte:fragment slot="lead">
 				<strong class="text-xl uppercase">HDC</strong>
 			</svelte:fragment>
@@ -50,9 +57,24 @@
 				>
 					Something Else
 				</a>
-			</svelte:fragment>
+            </svelte:fragment>
+            <svelte:fragment slot="trail">
+                <button use:popup={popupCLick}>
+                    <Avatar src="invalid-image.jpg" initials="FR" width="w-10" class="mr-0 p-0"/>
+                </button>
+            </svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
+
+
+<div class="card p-4 -my-1 variant-filled-background" data-popup="popupClick">
+<div class="grid grid-cols-1 gap-2">
+	<a href="/dashboard">Profile</a>
+	<a href="/">Settings</a>
+	<a href="/">Admin</a>
+</div>
+	<div class="arrow variant-filled-primary"/>
+</div>
